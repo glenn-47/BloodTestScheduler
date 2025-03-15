5aa9e4dab2f3e9adedaccf1278f726bba92cf6b7
glenn-47 <g47doyle@gmail.com>
2025-03-15 22:57:36 +0000

    interface class
    created interface class called Schedulable to compute a score for priority

---
+++ src/bloodtestscheduler/Schedulable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package bloodtestscheduler;
+
+/**
+ *
+ * @author glenn
+ */
+public interface Schedulable {
+    /**
+     * Computes and returns a priority score.
+     * @return 
+     */
+    int getPriorityScore();
+}
